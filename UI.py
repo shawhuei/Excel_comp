60bc37d68d8df536846bc8a0c75f0d3bee28d36f
shaohui <shawhuei@126.com>
2017-11-18 16:55:16 +0800

    Add basic UI

--- UI.py
+++ UI.py
@@ -1,7 +1,33 @@
 #!/usr/bin/env python 
+
 import wx
 
+class MainFrame(wx.Frame):
+	def __init__(self,parent,title):
+		wx.Frame.__init__(self,parent,title=title,size=(300,200))
+		#self.control=wx.TextCtrl(self,style=wx.TE_MULTILINE)
+		
+		self.CreateStatusBar()
+		filemenu=wx.Menu()
+		
+		
+		menuItem = filemenu.Append(wx.ID_ABOUT,"&About","Designed By Sam")
+		self.Bind(wx.EVT_MENU,self.OnAbout,menuItem)
+		filemenu.AppendSeparator()
+		filemenu.Append(wx.ID_EXIT,"&Exit","Exit")
+		
+		menuBar=wx.MenuBar()
+		menuBar.Append(filemenu,"&File")
+		self.SetMenuBar(menuBar)
+		
+		
+		self.Show(True)
+		
+	def OnAbout(self,event):
+		print("About event!")
+		
+		
+
 app = wx.App(False)
-frame=wx.Frame(None,wx.ID_ANY,"Hey")
-frame.Show(True)
+frame=MainFrame(None,'Hey')
 app.MainLoop()
